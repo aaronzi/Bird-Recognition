0bcac1c9d81d6764ff903d6dc4a98e2ca6c19420
Aaron Zielstorff <aaron.zi@web.de>
2023-12-13 17:24:45 +0100

    added option to detect single birds

--- app.py
+++ app.py
@@ -14,23 +14,40 @@ ns = api.namespace('birds', description='Bird operations')
 upload_parser = reqparse.RequestParser()
 upload_parser.add_argument('image', type=werkzeug.datastructures.FileStorage, location='files', required=True, help='Image file')
 
-# Response model
-response_model = api.model('Response', {
+# Response model for image containing multiple birds
+response_model_multi = api.model('Response_Multi', {
     'detected_birds': fields.List(fields.String, description='The identified birds'),
 })
 
+# Response model for image containing a single bird
+response_model_single = api.model('Response_Single', {
+    'detected_bird': fields.String(description='The identified bird'),
+})
 
-@ns.route('/identify')
+@ns.route('/identify-multi')
 @api.expect(upload_parser)
-class BirdIdentification(Resource):
-    @api.response(200, 'Success', response_model)
+class BirdIdentificationMulti(Resource):
+    @api.response(200, 'Success', response_model_multi)
     @api.response(400, 'Validation Error')
-    @api.doc(description="Identify birds in an uploaded image.")
+    @api.doc(description="Identify birds in an uploaded image. This first detects birds in the image and then identifies each detected bird.")
     def post(self):
         args = upload_parser.parse_args()
         image = args['image']
-        result = main.process_image(image)
+        result = main.process_image_multi(image)
         return {'detected_birds': result}
+    
+
+@ns.route('/identify-single')
+@api.expect(upload_parser)
+class BirdIdentificationSingle(Resource):
+    @api.response(200, 'Success', response_model_single)
+    @api.response(400, 'Validation Error')
+    @api.doc(description="Identify a single bird in an uploaded image. This takes the uploaded image and identifies the bird in the image.")
+    def post(self):
+        args = upload_parser.parse_args()
+        image = args['image']
+        result = main.process_image_single(image)
+        return {'detected_bird': result}
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import detection
 import identification
 
 
-def process_image(image):
+def process_image_multi(image):
     """
-    Process the uploaded image to detect and identify birds.
+    Process the uploaded image to detect and identify multiple birds.
 
     :param image: An image file.
     :return: List of identified bird species.
@@ -14,7 +14,23 @@ def process_image(image):
     detected_birds_images = detection.detect_birds(image)
 
     # Step 2: Identify Each Detected Bird
-    # Assuming identification.identify_bird returns the species of a given bird image
     identified_birds = [identification.identify_bird(bird_image) for bird_image in detected_birds_images]
 
     return identified_birds
+
+
+def process_image_single(image):
+    """
+    Process the uploaded image to detect and identify a single bird.
+
+    :param image: An image file.
+    :return: Identified bird species.
+    """
+
+    # Step 1: convert image to numpy array
+    image = detection.read_image(image)
+
+    # Step 2: Identify Bird
+    identified_bird = identification.identify_bird(image)
+
+    return identified_bird
